353ea10f547e647113559fa7dd4b0c76432a6cd5
muirurikin <muirurikin@gmail.com>
2019-03-27 22:44:35 +0300

    Fix config setup

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const Koa = require('koa');
 const router = require('koa-router');
-const config = require('./config/default');
+const config = require('config');
 const app = new Koa();
 const route = new router();
 
-let PORT = config.developmentConfig.PORT || 4000;
+let PORT = config.get('PORT');
 
 route.get('/', ctx => { ctx.body = 'Hello Koa'});
 
 app.use(route.routes());
 app.use(route.allowedMethods());
 
-let server = app.listen(PORT);
+let server = app.listen(PORT, () => {
+  console.log(`Koa app started on port ${PORT}`)
+});
+
 
