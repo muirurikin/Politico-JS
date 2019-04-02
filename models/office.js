d84a7c66a22085081c49310dd813363360592690
muirurikin <muirurikin@gmail.com>
2019-04-02 12:34:31 +0300

    Add create office method

--- models/office.js
+++ models/office.js
@@ -11,4 +11,15 @@ class Office {
   findById() {
     return this.offices.find(office => office.id === id);
   }
+
+  create(data) {
+    const { name, type } = data
+    let newOffice = {
+      id: this.offices.length + 1,
+      name,
+      type,
+    }
+
+    this.offices.push(newOffice);
+  }
 }
